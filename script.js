c5b5948c292bc4ceef8bdf2919bc081b10dc00c0
computer1011 <computadorespinosa@gmail.com>
2025-05-10 12:57:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-function abrirCarta() {
-  document.querySelector('.envelope').classList.toggle('abierta');
+function abrirCarta(event) {
+  event.stopPropagation(); // Evita que se dispare en otros elementos
+  document.getElementById('envelope').classList.toggle('abierta');
 }
+
